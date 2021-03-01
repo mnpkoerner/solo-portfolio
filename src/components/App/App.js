ed0372e1f50864b00f946788afeb8b7272042b90
mnpkoerner <mnpkoerner@gmail.com>
2021-02-28 22:22:22 -0600

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="photo-container">
+
+        <div className="link-panel">
+          <a href="https://www.linkedin.com/in/mnpkoerner/">
+          <p>Mark Koerner</p>
+          </a>
+          <a href="https://github.com/mnpkoerner">
+          <p>Full-Stack Developer</p>
+          </a>
+
+        </div>
+
+        <img className="photo" src="./images/self_port_web.png" alt="self portrait" />
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
